6438fdcd93575e78c08e569076b65ba62feea6e1
Mainul Hassan Main <shamim.moeen@gmail.com>
2021-06-04 22:06:45 +0600

    Change the tags canvas size according to window size and add new tag

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -2,11 +2,19 @@ import React from 'react';
 import TagsCanvas from 'react-tags-canvas';
 
 const Tags = () => {
-	const size = 600;
+	const width = window.innerWidth;
+
+	let size = 500;
+	let textHeight = 16;
+
+	if (width >= 1536) {
+		size = 600;
+		textHeight = 18;
+	}
 
 	return (
 		<TagsCanvas
-			className="text-gray-600 max-w-full"
+			className="text-indigo-500 max-w-full"
 			textColour={null}
 			freezeActive
 			shuffleTags
@@ -18,41 +26,43 @@ const Tags = () => {
 			wheelZoom={false}
 			width={size}
 			height={size}
-			weight={true}
-			weightMode={'size'}
+			weight={false}
+			textHeight={textHeight}
+			depth={0.7}
 			maxSpeed={0.05}
 			initial={[-0.17, 0.1]}
 			fadeIn={500}
 			tags={[
-				{ value: 'HTML5', weight: 18 },
-				{ value: 'CSS3', weight: 18 },
-				{ value: 'Bootstrap', weight: 18 },
-				{ value: 'jQuery', weight: 18 },
-				{ value: 'PHP', weight: 24 },
-				{ value: 'MySQL', weight: 24 },
-				{ value: 'WordPress', weight: 25 },
-				{ value: 'Git', weight: 24 },
-				{ value: 'Linux', weight: 20 },
-				{ value: 'Paypal', weight: 15 },
-				{ value: 'Stripe', weight: 15 },
-				{ value: 'REST', weight: 21 },
-				{ value: 'JSON', weight: 18 },
-				{ value: 'SCSS', weight: 21 },
-				{ value: 'Gulp', weight: 15 },
-				{ value: 'Docker', weight: 20 },
-				{ value: 'Javascript', weight: 18 },
-				{ value: 'npm', weight: 14 },
-				{ value: 'React', weight: 24 },
-				{ value: 'NodeJS', weight: 18 },
-				{ value: 'ExpressJS', weight: 16 },
-				{ value: 'Heroku', weight: 14 },
-				{ value: 'Netlify', weight: 14 },
-				{ value: 'Lodash', weight: 14 },
-				{ value: 'Redux', weight: 18 },
-				{ value: 'MongoDB', weight: 21 },
-				{ value: 'BrowserSync', weight: 15 },
-				{ value: 'Gutenberg', weight: 18 },
-				{ value: 'Tailwindcss', weight: 20 },
+				{ value: 'HTML5' },
+				{ value: 'CSS3' },
+				{ value: 'Bootstrap' },
+				{ value: 'jQuery' },
+				{ value: 'PHP' },
+				{ value: 'MySQL' },
+				{ value: 'WordPress' },
+				{ value: 'Git' },
+				{ value: 'Linux' },
+				{ value: 'Paypal' },
+				{ value: 'Stripe' },
+				{ value: 'REST' },
+				{ value: 'JSON' },
+				{ value: 'SCSS' },
+				{ value: 'Gulp' },
+				{ value: 'Webpack' },
+				{ value: 'Docker' },
+				{ value: 'Javascript' },
+				{ value: 'npm' },
+				{ value: 'React' },
+				{ value: 'NodeJS' },
+				{ value: 'ExpressJS' },
+				{ value: 'Heroku' },
+				{ value: 'Netlify' },
+				{ value: 'Lodash' },
+				{ value: 'Redux' },
+				{ value: 'MongoDB' },
+				{ value: 'BrowserSync' },
+				{ value: 'Gutenberg' },
+				{ value: 'Tailwindcss' },
 			]}
 		/>
 	);
